d113f41d810f1adba22967deef989a8e2997d860
André Posmitny <andre.lifter@gmail.com>
2024-10-03 15:12:47 +0100

    Integrate Material-UI and style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 // src/components/Header.js
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function Header() {
   return (
-    <header>
-      <h1>My React Portal</h1>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My React Portal
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
